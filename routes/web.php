ace8a2e3a55283eb1a1d8343396ab77db963a16e
Rafael <aciolyr@gmail.com>
2016-11-29 15:09:01 -0200

    comentarios nas rotas

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**
+ * Rotas responsaveis pela votação Seal Sistemas
+ */
 Route::group(['middleware' => 'sealVote'], function () {
     Route::get('votacao/seal/{email}', 'SealController@index');
 });
-
 Route::post('votacao/seal/enviar', 'SealController@update');
 
 
+/**
+ * Rotas resposnaveis pela votação Seal Telecom
+ */
 Route::group(['middleware' => 'sealTelecomVote'], function () {
     Route::get('votacao/sealtelecom/{email}', 'SealTelecomController@index');
 });
-
 Route::post('votacao/sealtelecom/enviar', 'SealTelecomController@update');
+
