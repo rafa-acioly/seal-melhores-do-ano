2e6df89f27749c45a63313b23b02e55c7284cd87
Rafael <aciolyr@gmail.com>
2016-11-29 13:49:43 -0200

    mensagem de artisan down alterada

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,10 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">
+                    Votação encerrada! <br>
+                    <small>Obrigado pela participação</small>
+                </div>
             </div>
         </div>
     </body>
