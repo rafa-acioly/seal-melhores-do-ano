484ec38e89820d4159d23ba6c27fe6cb30e19247
Rafeal <aciolyr@gmail.com>
2017-11-26 12:16:22 -0200

    finalização seal telecom

--- resources/views/sealTelecom/index.blade.php
+++ resources/views/sealTelecom/index.blade.php
@@ -14,48 +14,64 @@
     <div class="hero-head">
         <div class="logos">
             <div class="logo">
-                <img src="http://placehold.it/100x100" alt="" class="image">
+                <img src="{{ asset('template/images/icon1.png') }}" alt="" class="image is-96x96">
             </div>
             <div class="logo">
-                <img src="http://placehold.it/100x100" alt="" class="image">
+                <img src="{{ asset('template/images/icon2.png') }}" alt="" class="image is-100x100">
             </div>
         </div>
     </div>
     <div class="hero-body">
-        <div class="columns">
-            <div class="column is-half has-text-centered word-play">
-                <figure>
-                    <img src="http://placehold.it/400x200" alt="">
-                </figure>
-            </div>
-            <div class="column has-text-centered">
-                <h1 class="title is-1">Escolha seu voto</h1>
-                <form action="/votacao/sealtelecom/enviar" method="post">
-                    {!! csrf_field() !!}
-                    <input type="hidden" value="{{ $voter }}" name="voter">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6 has-text-centered word-play">
+                    <figure>
+                        <img src="{{ asset('template/images/SEAL_TELECOM_PESQUISA_MELHORES_DO_ANO_2017_INTERFACE_VOTACAO_1920X1080P-04.png') }}" alt="" class="image">
+                    </figure>
+                </div>
+                <div class="column is-6 has-text-centered">
+                    <h1 class="title is-1">Escolha seu voto</h1>
+                    <form action="/votacao/sealtelecom/enviar" method="post">
+                        {!! csrf_field() !!}
+                        <input type="hidden" value="{{ $voter }}" name="voter">
 
-                    <div class="field is-horizontal">
-                        <div class="field-body">
-                            <div class="field is-narrow">
-                                <div class="control">
-                                    <label for="" class="label">Comprometimento</label>
-                                    <div class="select">
-                                        <select name="commitment" required>
-                                            <option value="" selected disabled>Selecione</option>
-                                            @foreach($participants as $participant)
-                                                <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
-                                            @endforeach
-                                        </select>
+                        <div class="field is-horizontal">
+                            <div class="field-body">
+                                <div class="field is-narrow">
+                                    <div class="control">
+                                        <label for="" class="label">Comprometimento</label>
+                                        <div class="select">
+                                            <select name="commitment" required>
+                                                <option value="" selected disabled>Selecione</option>
+                                                @foreach($participants as $participant)
+                                                    <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="field is-narrow">
-                                <div class="control">
-                                    <label for="" class="label">Pró-atividade</label>
-                                    <div class="select">
+                                <div class="field is-narrow">
+                                    <div class="control">
+                                        <label for="" class="label">Pró-atividade</label>
                                         <div class="select">
-                                            <select name="proActivity" required>
+                                            <div class="select">
+                                                <select name="proActivity" required>
+                                                    <option value="" selected disabled>Selecione</option>
+                                                    @foreach($participants as $participant)
+                                                        <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="field is-narrow">
+                                    <div class="control">
+                                        <label for="" class="label">Superação</label>
+                                        <div class="select">
+                                            <select name="superation" required>
                                                 <option value="" selected disabled>Selecione</option>
                                                 @foreach($participants as $participant)
                                                     <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
@@ -65,58 +81,44 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="field is-narrow">
-                                <div class="control">
-                                    <label for="" class="label">Superação</label>
-                                    <div class="select">
-                                        <select name="superation" required>
-                                            <option value="" selected disabled>Selecione</option>
-                                            @foreach($participants as $participant)
-                                                <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
-                                            @endforeach
-                                        </select>
+                        <div class="field is-horizontal">
+                            <div class="field-body">
+                                <div class="field is-narrow">
+                                    <div class="control">
+                                        <label for="" class="label">Trabalho em equipe</label>
+                                        <div class="select">
+                                            <select name="teamWork" required>
+                                                <option value="" selected disabled>Selecione</option>
+                                                @foreach($participants as $participant)
+                                                    <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="field is-narrow">
+                                    <div class="control">
+                                        <label for="" class="label">Planejamento e organização</label>
+                                        <div class="select">
+                                            <select name="planningAndOrganization" required>
+                                                <option value="" selected disabled>Selecione</option>
+                                                @foreach($participants as $participant)
+                                                    <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="field is-horizontal">
-                        <div class="field-body">
-                            <div class="field is-narrow">
-                                <div class="control">
-                                    <label for="" class="label">Trabalho em equipe</label>
-                                    <div class="select">
-                                        <select name="teamWork" required>
-                                            <option value="" selected disabled>Selecione</option>
-                                            @foreach($participants as $participant)
-                                                <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="field is-narrow">
-                                <div class="control">
-                                    <label for="" class="label">Planejamento e organização</label>
-                                    <div class="select">
-                                        <select name="planningAndOrganization" required>
-                                            <option value="" selected disabled>Selecione</option>
-                                            @foreach($participants as $participant)
-                                                <option value="{{ $participant->id }}">{{ $participant->name }} - {{ $participant->department }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="field field-button">
+                            <button class="button is-medium" type="submit">Enviar</button>
                         </div>
-                    </div>
-                    <div class="field field-button">
-                        <button class="button is-medium" type="submit">Enviar</button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
